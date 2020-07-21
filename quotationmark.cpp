9b2f775d3b224302eac34721b1ffd1b6762cb5c8
Yuvraj Singh <44363932+yuvraj2711@users.noreply.github.com>
2020-07-21 11:39:54 +0530

    Create quotationmark.cpp

---
+++ quotationmark.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "\"QUOTE\"";
+cout<<"\\n\t"; //  "\\" produces "\" and then "n" is recognized as an ordinary symbol . And the next \t is for tab space.
+cout<<"\\t"; // output will be \t.
+    return 0;
+}
