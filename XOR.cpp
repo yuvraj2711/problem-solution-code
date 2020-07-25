0b5f3638eff58eee7df1dbd68229490a43c7236c
Yuvraj Singh <44363932+yuvraj2711@users.noreply.github.com>
2020-07-25 14:23:29 +0530

    Update and rename qrcode.py to XOR.cpp

---
+++ XOR.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a = 562;
+   int b = 654;
+   if ( (a ^ b) )
+      cout<<"Not Equal";
+   else
+      cout<<"Equal";
+      return 0;
+}
